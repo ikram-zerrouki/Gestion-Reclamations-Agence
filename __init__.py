254b04aed3fb892e4fecc8b54358143e6a1fc6c5
ikram-zerrouki <ki_zerrouki@esi.dz>
2025-03-25 23:22:52 +0100

    new commit

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*- 
+from . import controllers 
+from . import models 
+from .models import reclamation 
+from .models import equipe 
+from .models import traitement 
+from .models import deplacement
+
+
